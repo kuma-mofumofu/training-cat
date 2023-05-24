c276c52391093e07925aa055365f5d5608150a5d
kuma-mofumofu <toucyann.910@gmail.com>
2023-05-24 16:51:32 +0900

    終了時間のコミット

--- js/script.js
+++ js/script.js
@@ -11,6 +11,35 @@ $(function () {
     limit.textContent = str.substring(0, len) + "…";
   }
 
-  
+
+  /* ===============================================
+  # ページtopボタンをスクロールで移動させる
+  =============================================== */
+  $('.page_top--inner').on("click", function(){
+
+    const speed = 500;// スクロールの速度
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? 'html' : href);
+    const position = target.offset().top;
+
+    $('html').animate({scrollTop: position}, speed, 'swing');
+
+    return false;
+  });
+
+
+  /* ===============================================
+  # ハンバーガー
+  =============================================== */
+  $(".burger").on("click",function(){
+
+    $(this).toggleClass("is-open");
+
+    $(".open_menu").toggleClass("is-open");
+    $("#header").toggleClass("is-open");
+    $("main").toggleClass("is-open");
+    $("#footer").toggleClass("is-open");
+    $(".privacy").toggleClass("is-open");
+  });
 
 });
